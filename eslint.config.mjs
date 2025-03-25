12e15b6e16ed5649395e9ec52ad70c3d524aab8b
RohSungKyun <songkyun1108@naver.com>
2025-03-25 13:23:05 +0900

    [feat] #noissue : eslint와 prettier 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      semi: ["warn", "always"], // 세미콜론 추가
+      quotes: ["error", "double"], // 더블 따옴표 사용
+      "no-unused-vars": "warn", // 사용되지 않는 변수 경고
+      "react/react-in-jsx-scope": "off", // React JSX scope 관련 경고 끄기
+      "@typescript-eslint/no-explicit-any": "off", // `any` 타입 사용 허용
+      "react/prop-types": "off", // prop-types 사용하지 않기
+      "@typescript-eslint/no-unused-vars": "warn", // 사용되지 않는 변수 경고
+      "react/no-unused-state": "error", // 사용되지 않는 state는 에러
+      "array-callback-return": "off", // array-callback-return 규칙 끄기
+      "react/self-closing-comp": "warn", // self-closing tag에 대한 경고
+      "@typescript-eslint/ban-ts-comment": "off", // ts-comment 관련 규칙 끄기
+    },
+  },
 ];
+
 
 export default eslintConfig;
---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports  = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'always',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  endOfLine: 'auto',
+};
